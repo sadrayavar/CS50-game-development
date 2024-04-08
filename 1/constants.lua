b6448c12b1d554a7cd765c408dbfb9654fe2e653
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-04-09 00:10:31 +0330

    day-0 update

---
+++ 1/constants.lua
@@ -0,0 +1,8 @@
+WINDOW_HEIGTH = 768
+WINDOW_WIDTH = 1366
+
+WINDOW_MODE = {
+    fullscreen = true,
+    resizable = false,
+    vsync = true
+}
--- 1/main.lua
+++ 1/main.lua
@@ -1,3 +1,18 @@
+require 'constants'
+
+function love.load()
+    love.window.setTitle("Hello World")
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGTH, WINDOW_MODE)
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 499, 399)
+    local text = "Hello World!"
+
+    local font_width = love.graphics.getFont():getWidth(text)
+    local font_height = love.graphics.getFont():getHeight()
+
+    local x = WINDOW_WIDTH / 2 - font_width / 2
+    local y = WINDOW_HEIGTH / 2 - font_height / 2
+
+    love.graphics.print(text, x, y)
 end
