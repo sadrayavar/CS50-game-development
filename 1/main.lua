0d3d1460864f202ce47b90d40dfa0f02fdc34bb3
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-04-09 00:28:25 +0330

    day-1 update: filtering text

--- 1/main.lua
+++ 1/main.lua
@@ -1,18 +1,30 @@
 require 'constants'
+push = require 'push'
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle("Hello World")
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGTH, WINDOW_MODE)
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGTH, WINDOW_WIDTH, WINDOW_HEIGTH, WINDOW_MODE)
 end
 
 function love.draw()
-    local text = "Hello World!"
+    push:start()
+
+    local text = "Do I like this?"
 
     local font_width = love.graphics.getFont():getWidth(text)
     local font_height = love.graphics.getFont():getHeight()
 
-    local x = WINDOW_WIDTH / 2 - font_width / 2
-    local y = WINDOW_HEIGTH / 2 - font_height / 2
+    local x = VIRTUAL_WIDTH / 2 - font_width / 2
+    local y = VIRTUAL_HEIGTH / 2 - font_height / 2
 
     love.graphics.print(text, x, y)
+
+    push:finish()
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
