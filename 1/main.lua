2361cd5746d95f9b4721e7896bb82ef0b7bcd1c6
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-04-09 20:15:14 +0330

    day-2 update: init setup

--- 1/main.lua
+++ 1/main.lua
@@ -5,20 +5,41 @@ function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle("Hello World")
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGTH, WINDOW_WIDTH, WINDOW_HEIGTH, WINDOW_MODE)
+
 end
 
 function love.draw()
     push:start()
 
+    -- background color
+    love.graphics.clear(3, 0, 20, 255)
+
+    -- Do I like this?
+    love.graphics.setFont(love.graphics.newFont("CuteEasterPersonalUse-Wy8nV.ttf", 12))
     local text = "Do I like this?"
-
+    local font_heigth = love.graphics.getFont():getHeight()
     local font_width = love.graphics.getFont():getWidth(text)
-    local font_height = love.graphics.getFont():getHeight()
+    local font_pos = {
+        x = VIRTUAL_WIDTH / 2 - font_width / 2,
+        y = 0
+    }
+    love.graphics.print(text, font_pos.x, font_pos.y)
 
-    local x = VIRTUAL_WIDTH / 2 - font_width / 2
-    local y = VIRTUAL_HEIGTH / 2 - font_height / 2
+    -- menu_line
+    local line_heigth = 1
+    love.graphics.rectangle('fill', 0, font_heigth, VIRTUAL_WIDTH, line_heigth)
 
-    love.graphics.print(text, x, y)
+    -- players
+    local player_width = 20
+    local player_heigth = 3
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - player_width / 2, font_heigth + line_heigth, player_width,
+        player_heigth)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - player_width / 2, VIRTUAL_HEIGTH - player_heigth, player_width,
+        player_heigth)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, (VIRTUAL_HEIGTH + font_heigth + line_heigth) / 2 - 2, 4, 4)
 
     push:finish()
 end
