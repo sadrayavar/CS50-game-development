9408c3ed2a07327218a0b9536f95082661f070c6
sadrayavar <sadra.yavarzadeh@gmail.com>
2024-07-08 19:59:07 +0330

    Moved main_funcs to dep folder

--- 1- fifty-bird/dep/states/CountdownState.lua
+++ 1- fifty-bird/dep/states/CountdownState.lua
@@ -3,11 +3,25 @@ CountdownState = Class {
 }
 
 function CountdownState:init()
-
+    countdwonNumber = 3
+    virtualTimePerSec = 0.5
+    countdownTimer = countdwonNumber + 1
 end
 
-function CountdownState:update()
+function CountdownState:update(dt)
+    countdownTimer = countdownTimer + dt
+
+    if countdownTimer >= countdwonNumber then
+
+        countdownTimer = 0
+    end
 end
 
 function CountdownState:render()
+    love.graphics.setColor(0 / 255, 0 / 255, 0 / 255)
+
+    -- printing game name
+    local x = (VIRTUAL_WIDTH - fonts.cubic:getWidth(countdwonNumber)) / 2
+    local y = VIRTUAL_HEIGHT / 4
+    love.graphics.print(countdwonNumber, fonts.fancy, x, y)
 end
